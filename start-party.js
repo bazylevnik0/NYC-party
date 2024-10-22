f37d9523a0dabac55c06c60bf975f1adbb2cbabe
niko bazylev <42851684+bazylevnik0@users.noreply.github.com>
2024-10-22 15:16:29 +0000

    add guests' enter/exit functionality

--- start-party.js
+++ start-party.js
@@ -16,15 +16,39 @@ app.get("/enter", (req, res) => {
   res.sendFile("/public/pages/enter.html", op);
 });
 
+app.get("/exit", (req, res) => {
+  let guest = new Guest(req.query.nickname, req.query.avatar);
+  let check = false;
+  for (let i = 0; i < guests.length; i++) {
+    if (guest.nickname === guests[i].nickname) {
+      guests.splice(i, 1);
+    }
+  }
+  res.sendFile("/public/pages/enter.html", op);
+  console.log(guests);
+});
+
 app.get("/campfire", (req, res) => {
+  let guest = new Guest(req.query.nickname, req.query.avatar);
+  let check = false;
+  for (let i = 0; i < guests.length; i++) {
+    if (guest.nickname === guests[i].nickname) {
+      check = true;
+    }
+  }
+  if (!check) guests.push(guest);
   res.sendFile("/public/pages/campfire.html", op);
-  console.log("-------------------");
-  console.log("entered new guest:");
-  console.log("nickname:", req.query.nickname);
-  console.log("avatar:", req.query.avatar);
-  console.log("-------------------");
+  console.log(guests);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+var guests = [];
+class Guest {
+  constructor(nickname, avatar) {
+    this.nickname = nickname;
+    this.avatar = avatar;
+  }
+}
