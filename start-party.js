5eaa8622f7e417d8dd5bfc8f84bd4963d6ac1e7b
bazylevnik0 <bazylevnik0@gmail.com>
2024-10-25 21:53:03 +0300

    refactoring a: adding comments

--- start-party.js
+++ start-party.js
@@ -8,6 +8,8 @@ const path = require("path");
 const root = {
   root: path.join(__dirname),
 };
+var passphrase = "42"; //default
+if (process.argv[2]) passphrase = ""+process.argv[2]
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
@@ -29,14 +31,12 @@ class Guest {
 }
 
 app.get("/", (req, res) => {
-  console.log( "GET: /");
   res.sendFile("/public/enter.html", root);
 });
 
 app.post("/join", (req, res) => {
-  console.log("POST: /join, nickname: ", req.body.nickname);
-  console.log("POST: /join, avatar: "  , req.body.avatar);
   res.redirect('/campfire?nickname='+req.body.nickname+'&avatar='+req.body.avatar+'&id='+Math.random());
+  // creates random id when joining
 });
 
 app.get("/campfire", (req, res) => {
@@ -44,34 +44,33 @@ app.get("/campfire", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  console.log("POST: /register, nickname: "   , req.body.nickname);
-  console.log("POST: /register, avatar: "     , req.body.avatar);
-  console.log("POST: /register, id: "         , req.body.id);
-  console.log("POST: /register, public_key: " , req.body.public_key);
   let guest = new Guest(req.body.nickname, req.body.avatar, req.body.id, req.body.public_key);
   guests.push(guest);
-  console.log("/POST /register, guests: ", guests);
+  // after registration guest has not only id but public key
+  // generating of guest keys on the client side
+  console.log(guests)
 });
 
 app.post("/exit", (req, res) => {
-  console.log("POST: /exit, id: "     ,req.body.id);
-  console.log("POST: /exit, message: ", req.body.message);
+  // Parse request and the sign of request
   let id = req.body.id;
   let detachedSignature = JSON.parse(req.body.message);
   (async () => {
+  // Find public key of the guest who requested
   let guestId = id;
   let guestPublicKey;
   let guest_number = 0;
-  for (let i = 0; i < guests.length; i++) {
+  for (let i = 0; i < guests.length; i++) { // in guests
     if (guestId == guests[i].id) {
       guestPublicKey = guests[i].public_key;
       guest_number = i;
     }
   }
+  // Verify request by signature
   if (guestPublicKey) {
     const message = await openpgp.createMessage({ text: 'exit' });
     const signature = await openpgp.readSignature({
-        armoredSignature: detachedSignature // parse detached signature
+        armoredSignature: detachedSignature
     });
     const publicKey = await openpgp.readKey({ armoredKey: guestPublicKey });
     const verificationResult = await openpgp.verify({
@@ -81,12 +80,11 @@ app.post("/exit", (req, res) => {
     })
     const { verified, keyID } = verificationResult.signatures[0];
     try {
-        await verified; // throws on invalid signature
-        console.log('Signed by key id ' + keyID.toHex());
+        await verified;
+        // If verified delete the guest from guests
         if (verified) {
           guests.splice(guest_number,1);
-          console.log(guests);
-          console.log(verified);
+          console.log(guests)
         }
     } catch (e) {
         throw new Error('Signature could not be verified: ' + e.message);
