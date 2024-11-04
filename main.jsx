76f8022af269cc47dd3f2a0dec41374f43615b1b
bazylevnik0 <bazylevnik0@gmail.com>
2024-11-05 00:45:45 +0300

    add react+express template by vite

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css'
+
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>LETS PARTY!</h1>);
