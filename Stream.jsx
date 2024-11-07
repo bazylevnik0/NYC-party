9d117140d5c964111cf2b0a8185e0ef5cd47c24f
bazylevnik0 <bazylevnik0@gmail.com>
2024-11-07 03:31:17 +0300

    continue to add video

--- Stream.jsx
+++ Stream.jsx
@@ -6,30 +6,32 @@ export default function Stream() {
 
     const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
     const detectorConfig = {
-    runtime: 'mediapipe',
-    solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-                    // or 'base/node_modules/@mediapipe/face_mesh' in npm.
+        runtime: 'mediapipe',
+        solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
     };
-    (async ()=> {
-        detector = await faceLandmarksDetection.createDetector(model, detectorConfig);
-    })
+        let detector = faceLandmarksDetection.createDetector(model, detectorConfig);
+        var video = document.createElement('video');
+        video.autoplay = true;
 
-
-    var video = document.createElement('video');
-    video.autoplay = true;
-
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia(
-        {
-            video: true,
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia(
+            {
+                video: true,
+            })
+            // If success
+            .then(async (stream) => {
+                let detector_temp = await detector;
+                video.srcObject = stream;
+                setInterval(()=>{
+                    const estimationConfig = {flipHorizontal: false};
+                    (async()=>{
+                        const faces = await detector_temp.estimateFaces(video, estimationConfig);
+                        console.log(faces)
+                    })();
+            
+                },100)
+            })
         }
-        )
-        // If success
-        .then((stream) => {
-            video.srcObject = stream;
-            //document.body.appendChild(video);
-        })
-    }
-    
+
     return  <p>adsd</p>
 }
