82db007b4ed186e4bf8372fee32366eb82ea59e8
bazylevnik0 <bazylevnik0@gmail.com>
2024-11-10 02:25:55 +0300

    start rewrite by opencv

--- Stream.jsx
+++ Stream.jsx
@@ -7,6 +7,7 @@ import Skull from './Skull.jsx'
 
 
 export default function Stream(props) {
+    
     // Audio
     var socket = io();
 
@@ -58,18 +59,13 @@ export default function Stream(props) {
     });
 
     // Video 
-    const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
-    const detectorConfig = {
-        runtime: 'mediapipe',
-        solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
-    };
-    const detector = faceLandmarksDetection.createDetector(model, detectorConfig);
-    
+   
     const interval_video = useRef();
 
     let video = document.createElement('video');
         video.autoplay = true;
-
+    let canvas_temp = document.getElementById("canvas_temp");
+    let canvas_temp_ctx = canvas_temp.getContext("2d");
     const [rotation, setRotation] = useState([]);
     if (props.video.videoActive) {
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -78,19 +74,44 @@ export default function Stream(props) {
                 video: true,
             })
             // If success
-            .then(async (stream) => {
-                //[0].keypoints[7] rightEye
-                //[0].keypoints[263] leftEye
-                let detector_temp = await detector;
+            .then((stream) => {
                 video.srcObject = stream;
+                canvas_temp_ctx.drawImage(video, 0, 0, canvas_temp.width, canvas_temp.height);
                 interval_video.current = setInterval(()=>{
-                    const estimationConfig = {flipHorizontal: false};
-                    (async()=>{
-                        const faces = await detector_temp.estimateFaces(video, estimationConfig);
-                        //console.log("asd")
-                        if (faces && faces[0] && faces[0].keypoints ) setRotation([0,faces[0].keypoints[7].z/50,0])
-                        //console.log("left: ",faces[0].keypoints[7].z,"right: ",faces[0].keypoints[263].z);
-                    })();
+                    
+                    let src = cv.imread('canvas_temp');
+                    let gray = new cv.Mat();
+                    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+                    let faces = new cv.RectVector();
+                    let eyes = new cv.RectVector();
+                    let faceCascade = new cv.CascadeClassifier();
+                    let eyeCascade = new cv.CascadeClassifier();
+                    // load pre-trained classifiers
+                    faceCascade.load('haarcascade_frontalface_default.xml');
+                    eyeCascade.load('haarcascade_eye.xml');
+                    // detect faces
+                    let msize = new cv.Size(0, 0);
+                    faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+                    for (let i = 0; i < faces.size(); ++i) {
+                        let roiGray = gray.roi(faces.get(i));
+                        let roiSrc = src.roi(faces.get(i));
+                        /*
+                        let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
+                        let point2 = new cv.Point(faces.get(i).x + faces.get(i).width,
+                                                faces.get(i).y + faces.get(i).height);
+                        */
+                        // detect eyes in face ROI
+                        eyeCascade.detectMultiScale(roiGray, eyes);
+                        for (let j = 0; j < eyes.size(); ++j) {
+                            let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
+                            let point2 = new cv.Point(eyes.get(j).x + eyes.get(j).width,
+                                                    eyes.get(j).y + eyes.get(j).height);
+                          console.log("point1: ",point1, "point2: ", point2);
+                        }
+                        roiGray.delete(); roiSrc.delete();
+                    }
+
+                
                 },100)
                 
             })
@@ -99,11 +120,12 @@ export default function Stream(props) {
         clearInterval(interval_video.current);
     }
 
-    let answer_video_active = (<Canvas>
-                                <Skull rotation={rotation}/>
-                                <ambientLight intensity={0.1} />
-                                <directionalLight position={[0, 0, 5]} color="white" />
-                               </Canvas>);  
+    let answer_video_active = ( <Canvas>
+                                    <Skull rotation={rotation}/>
+                                    <ambientLight intensity={0.1} />
+                                    <directionalLight position={[0, 0, 5]} color="white" />
+                                </Canvas>
+                              );  
 
     let answer_video_inactive = (<div>none</div>)
 
