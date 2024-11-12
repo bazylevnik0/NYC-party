cfa7be497009cdf0d67ad9ad181fd9a2c62659fb
bazylevnik0 <bazylevnik0@gmail.com>
2024-11-12 23:20:56 +0300

    opencv loaded(but alg is not working)

--- Stream.jsx
+++ Stream.jsx
@@ -66,6 +66,31 @@ export default function Stream(props) {
         video.autoplay = true;
     let canvas_temp = document.getElementById("canvas_temp");
     let canvas_temp_ctx = canvas_temp.getContext("2d");
+
+    let src = cv.imread('canvas_temp');
+    let gray = new cv.Mat();
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+    let faces = new cv.RectVector();
+    let eyes = new cv.RectVector();
+    
+    let faceCascade = new cv.CascadeClassifier();
+    let eyeCascade = new cv.CascadeClassifier();
+
+    (async () => {
+        let url_face = window.location.href+'haarcascade_frontalface_default.xml';
+        let url_eye = window.location.href+'haarcascade_eye.xml';
+              
+        try {
+            let response_face = await fetch(url_face);
+            faceCascade.load(await response_face.text());
+
+            let response_eye = await fetch(url_eye);
+            eyeCascade.load(await response_eye.text());
+        } catch (error) {
+            console.log(error)
+        }
+    })();
+
     const [rotation, setRotation] = useState([]);
     if (props.video.videoActive) {
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -78,53 +103,29 @@ export default function Stream(props) {
                 video.srcObject = stream;
                 canvas_temp_ctx.drawImage(video, 0, 0, canvas_temp.width, canvas_temp.height);
                 interval_video.current = setInterval(()=>{
-                    
-                    let src = cv.imread('canvas_temp');
-                    let gray = new cv.Mat();
-                    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
-                    let faces = new cv.RectVector();
-                    let eyes = new cv.RectVector();
-                    
-                    let faceCascade = new cv.CascadeClassifier();
-                    let eyeCascade = new cv.CascadeClassifier();
-                    (async () => {
-                        const url = window.location.href+'haarcascade_frontalface_default.xml';
-                        
-                        try {
-                          const response = await fetch(url);
-                          faceCascade.load(response.text());
-                        //eyeCascade .load(window.location.href+'haarcascade_eye.xml');
-                        
-                        // detect faces
-                        let msize = new cv.Size(0, 0);
-                        faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
-                        for (let i = 0; i < faces.size(); ++i) {
-                            let roiGray = gray.roi(faces.get(i));
-                            let roiSrc = src.roi(faces.get(i));
-                            /*
-                            let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-                            let point2 = new cv.Point(faces.get(i).x + faces.get(i).width,
-                                                    faces.get(i).y + faces.get(i).height);
-                            */
-                            // detect eyes in face ROI
-                            /*
-                            eyeCascade.detectMultiScale(roiGray, eyes);
-                            for (let j = 0; j < eyes.size(); ++j) {
-                                let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
-                                let point2 = new cv.Point(eyes.get(j).x + eyes.get(j).width,
-                                                          eyes.get(j).y + eyes.get(j).height);
-                              console.log("point1: ",point1, "point2: ", point2);
-                            }
-                            roiGray.delete(); roiSrc.delete();
-                            */
-                        }
-                         
-                        } catch (error) {
 
+                    // detect faces
+                    let msize = new cv.Size(0, 0);
+                    faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
+                    for (let i = 0; i < faces.size(); ++i) {
+                        let roiGray = gray.roi(faces.get(i));
+                        let roiSrc = src.roi(faces.get(i));
+                      
+                        let point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
+                        let point2 = new cv.Point(faces.get(i).x + faces.get(i).width,
+                                                faces.get(i).y + faces.get(i).height);
+                        // detect eyes in face ROI
+                        eyeCascade.detectMultiScale(roiGray, eyes);
+                        for (let j = 0; j < eyes.size(); ++j) {
+                            let point1 = new cv.Point(eyes.get(j).x, eyes.get(j).y);
+                            let point2 = new cv.Point(eyes.get(j).x + eyes.get(j).width,
+                                                      eyes.get(j).y + eyes.get(j).height);
+                          console.log("point1: ",point1, "point2: ", point2);
                         }
-                           
-                    })();
-                },100)
+                        roiGray.delete(); roiSrc.delete();
+                     }
+                      
+            },100)
                 
             })
         }
